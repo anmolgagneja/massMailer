866b9e27a9324e6bc4f2f1808a4229dba99318ca
anmolgagneja <anmol@thegeekbox.com>
2013-02-19 17:22:33 +0530

    trans mail with recipient variables

--- mailgun/index.php
+++ mailgun/index.php
@@ -4,7 +4,25 @@ if (isset($_POST['to']))
 {
 echo send_message($_POST['to'],$_POST['subject'],$_POST['htmlbody']);
 }
-sendMessageRecipVars();
+if (isset($_POST['towithrecip']))
+  {
+    $adresestring=trim($_POST['towithrecip']);
+    $adreses=explode(' ',$adresestring);
+    foreach ($adreses as $indadres)
+      {
+	$indadresarr=explode(',',$indadres);
+	$names[]=$indadresarr[0];
+	$mails[]=$indadresarr[1];
+      }
+    $tostring=implode(',',$mails);
+    foreach($mails as $key=>$xx)
+      {
+	$recipsdataarr[$xx]['first']=$names[$key];
+      }
+    $recipVarString=json_encode($recipsdataarr);
+    echo sendMessageRecipVars($tostring,$recipVarString,$_POST['subjectwithrecip'],$_POST['htmlbodywithrecip']);
+  }
+
 ?>
 <form action="index.php" method="post">
 To: (comma separated) <input type"text" name="to"><br/>
@@ -12,3 +30,13 @@ Subject: <input type="text" name="subject"/><br/>
 HTML Body: <input type="text" name="htmlbody"/><br/>
 <input type="submit"/>
 </form>
+
+Trans mail with recip variables
+  (use %recipient.first% for recipients first name)
+<form action="index.php" method="post">
+  To. (comma seperated and space seperated like Anmol,anmol@thegeekbox.com Vibhu,vibhutiwari321@gmail.com do not use spaces in first name) <input type"text" name="towithrecip"/><br/>
+Subject <input type="text" name="subjectwithrecip"/><br/>
+HTML Body <input type="text" name="htmlbodywithrecip"/></br/>
+<input type="submit"/>
+</form>
+  
--- mailgun/modules.php
+++ mailgun/modules.php
@@ -13,9 +13,9 @@ curl_close($curlini);
 return $result;
 }
 // function to send mail with recep vars
-function sendMessageRecipVars()
+function sendMessageRecipVars($tostring,$recipvarstring,$subject,$body)
 {
-ch = curl_init();
+  $ch = curl_init();
 
   curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
   curl_setopt($ch, CURLOPT_USERPWD, 'api:key-9ce6orly1j84gmsso9bxm-mw45bh43x5');
@@ -23,14 +23,16 @@ ch = curl_init();
 
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
   curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v2/anmolgagneja.mailgun.org/messages');
-curl_setopt($ch,
-              CURLOPT_POSTFIELDS,
-              array('from' => 'Excited User <me@samples.mailgun.org>',
-                    'to' => 'anmol@thegeekbox.com,vibhutiwari321@gnail.com',
-                    'subject' => 'Hello, %recipient.first%',
-                    'text' => 'If you wish to unsubscribe, click http://mailgun/unsubscribe/%recipient.id%',
-                    'recipient-variables' => '{"anmol@thegeekbox.com": {"first":"Anmol", "id":1}, "vibhutiwari321@gmail.com": {"first":"Vibhu", "id": 2}}'));
-
+  curl_setopt(
+      $ch,
+      CURLOPT_POSTFIELDS,
+      array(
+          'from' => 'Anmol <anmol@anmolgagneja.mailgun.org>',
+	  'to' => $tostring,
+	  'subject' => $subject,
+	  'html' => $body,
+	  'recipient-variables' => $recipvarstring));
+ 
   $result = curl_exec($ch);
   curl_close($ch);
 return $result;
