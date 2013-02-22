83bb92f08152eb1ef47bd3a3703d376df9b9c725
anmolgagneja <anmol@thegeekbox.com>
2013-02-22 11:05:40 +0530

    changing sender mail

--- mailgun/modules.php
+++ mailgun/modules.php
@@ -7,7 +7,7 @@ curl_setopt($curlini,CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 curl_setopt($curlini,CURLOPT_USERPWD,'api:key-9ce6orly1j84gmsso9bxm-mw45bh43x5');
 curl_setopt($curlini,CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($curlini,CURLOPT_URL,'https://api.mailgun.net/v2/anmolgagneja.mailgun.org/messages');
-curl_setopt($curlini,CURLOPT_POSTFIELDS,array ('from'=>'Anmol <postmaster@anmolgagneja.mailgun.org>','to'=>$to,'subject'=>$subject,'html'=>$htmlbody));
+curl_setopt($curlini,CURLOPT_POSTFIELDS,array ('from'=>'Saliraganar <email@saliraganar.com>','to'=>$to,'subject'=>$subject,'html'=>$htmlbody));
 $result=curl_exec($curlini);
 curl_close($curlini);
 return $result;
