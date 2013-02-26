8c0ac06daf9c179e0403a5803b3b1a1d01edcead
anmolgagneja <anmol@thegeekbox.com>
2013-02-26 11:18:13 +0530

    stats

---
+++ mailgun/stats.php
@@ -0,0 +1,12 @@
+<?php
+
+include 'modules.php';
+
+$opensJsonString=getOpens();
+$opensJsonArray=json_decode($opensJsonString,true);
+
+echo $opensJsonArray['items'][0]['total_count'];
+
+echo getClicks();
+
+?>
